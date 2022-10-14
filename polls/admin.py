2436a54f784a4a3d3f0a01e4f8567f524507aca2
leo <leosanches@uel.br>
2022-10-14 15:40:07 -0300

    django2

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Question, Post
 
 admin.site.register(Question)
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'slug', 'autor', 'publicado', 'status')
+    list_filter = ('status', 'criado', 'publicado', 'autor')
+    raw_id_fields = ('autor',)
+    date_hierarchy = 'publicado'
+    search_fields = ('titulo', 'conteudo')
+    prepopulated_fields = {'slug':('titulo',)}
 # Register your models here.
